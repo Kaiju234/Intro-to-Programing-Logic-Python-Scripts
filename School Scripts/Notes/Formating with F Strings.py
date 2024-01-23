68c49c9ad6790f13cf61e1c9415f87bf64535f7f
Raj Shah <68970395+Kaiju234@users.noreply.github.com>
2024-01-23 16:37:22 -0600

    Add files via upload

---
+++ School Scripts/Notes/Formating with F Strings.py
@@ -0,0 +1,8 @@
+#weight_oz = 9.50
+
+#print(f'The fish weighs {weight_oz:.2f} oz')	
+
+
+number = 9
+
+print(f'{number:.2f}, {number:d}, {number:03d}')
---
+++ School Scripts/Notes/String Split and Join.py
@@ -0,0 +1,5 @@
+path = '/Users/Jon'
+
+separator = '-'
+path_tokens = path.split('/')
+print(separator.join(path_tokens))
